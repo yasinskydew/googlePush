ce1044582548015a4b69db97366eec67d3e084c2
VladimirYasinsky <uladzimir.yasinski@webfriends.by>
2020-09-28 23:52:27 +0300

    add messages v11

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 const credential = {
   client_id:"343029256848-8cf71b6a3m9dktirbsbobiqqob9ss252.apps.googleusercontent.com",
-  refresh_token: "1//04oFVLodJSi_FCgYIARAAGAQSNwF-L9IrXxY0Ew-j-3pfo63LRZchjPRF9kE8hqyN2fkp7gQFuMsaiNu_djVPIWPB8PRCn4xm1tI",
+  refresh_token: "1//04WbDCMc_WpA4CgYIARAAGAQSNwF-L9IrDBFytXK5ir2LxJEGcUl2vFHRTNfU6H_F-FKVzcpre7-9ZbyWDUwNoZ3Kpq5h0Onuty0",
   client_secret:"jhQJznZjzIJUvCTmyJH3HKln",
   redirect_uris: ["https://developers.google.com/oauthplayground"],
 };
@@ -36,37 +36,45 @@ app.post('/push', (req, res) => {
     userId: 'me',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
-    console.log(res.data, 'res.data')
-    // gmail.users.messages.get({
-    //   userId: 'me',
-    //   id: res.data.history[0].messagesAdded[0].message.id
-    // }, (err2, res2) => {
-    //   if (err2) return console.log('The API returned an error: ' + err);
-    //   res2.data.payload.parts.map(i => {
-    //     console.log(Buffer.from(i.body.data, 'base64').toString())
-    //   })
-    //   console.log(res.data.payload.parts)
-    // })
+    if(res.data.history.length) {
+      const messages = res.data.history[0].messages;
+      const messagesAdd = res.data.history[0].messagesAdded;
+      if(messages.length) {
+        gmail.users.messages.get({
+          userId: 'me',
+          id: messages[0].id
+        }, (err2, res2) => {
+          if (err2) return console.log('The API returned an error: ' + err);
+          res2.data.payload.parts.map(i => {
+            console.log(Buffer.from(i.body.data, 'base64').toString())
+          })
+          console.log(res.data.payload.parts)
+        });
+      }
+      if(messagesAdd.length) {
+        gmail.users.messages.get({
+          userId: 'me',
+          id: messagesAdd[0].message.id
+        }, (err2, res2) => {
+          if (err2) return console.log('The API returned an error: ' + err);
+          res2.data.payload.parts.map(i => {
+            console.log(Buffer.from(i.body.data, 'base64').toString())
+          })
+          console.log(res.data.payload.parts)
+        })
+      }
+    }
   });
   res.send('Success');
 });
 app.get('/', function(req, res) {
-  // gmail.users.history.list({
-  //   startHistoryId: '3893',
-  //   userId: 'me',
-  // }, (err, res) => {
-  //   if (err) return console.log('The API returned an error: ' + err);
-  //   gmail.users.messages.get({
-  //     userId: 'me',
-  //     id: res.data.history[0].messagesAdded[0].message.id
-  //   }, (err, res) => {
-  //     if (err) return console.log('The API returned an error: ' + err);
-  //     res.data.payload.parts.map(i => {
-  //       console.log(Buffer.from(i.body.data, 'base64').toString())
-  //     })
-  //     console.log(res.data.payload.parts)
-  //   })
-  // });
+  gmail.users.history.list({
+    startHistoryId: '4802',
+    userId: 'me',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    console.log(res.data.history[0].messages[0])
+  });
   res.send('Backend Business Boutique is Ready');
 });
 const port = process.env.PORT || 3001;
